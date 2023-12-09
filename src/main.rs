0ffb5f185c47c938159c2cc7a1146abb8454da55
Claudio Carnino <claudio@tugulab.org>
2023-12-09 17:20:44 +0000

    refactor: using serde_bencode

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,22 @@
+use serde_bencode;
 use std::env;
-
-// Available if you need it!
-// use serde_bencode
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     let mut encoded_value_chars = encoded_value.chars();
     let encoded_value_first_char = encoded_value_chars.next().unwrap();
-    let encoded_value_last_char = encoded_value_chars.last().unwrap();
 
     if encoded_value_first_char.is_digit(10) {
         // If encoded_value starts with a digit, it's a string
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else if encoded_value_first_char.eq(&'i') && encoded_value_last_char.eq(&'e') {
+        let decoded_value: String = serde_bencode::de::from_str(&encoded_value)
+            .unwrap_or_else(|e| panic!("Failed to decode: {}. Error: {}", encoded_value, e));
+        return serde_json::Value::String(decoded_value.to_string());
+    } else if encoded_value_first_char.eq(&'i') {
         // It's an integer
-        let number_string = &encoded_value[1..(encoded_value.len() - 1)];
-        let number = number_string.parse::<i64>().unwrap();
-        return serde_json::Value::Number(serde_json::Number::from(number));
+        let decoded_value: i64 = serde_bencode::de::from_str(&encoded_value)
+            .unwrap_or_else(|e| panic!("Failed to decode: {}. Error: {}", encoded_value, e));
+        return serde_json::Value::Number(serde_json::Number::from(decoded_value));
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
