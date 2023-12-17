294c397a79c3a21919a345936deb76aded18c2df
Claudio Carnino <claudio@tugulab.org>
2023-12-17 15:57:21 +0000

    Refactored the main file

--- src/main.rs
+++ src/main.rs
@@ -25,45 +25,64 @@ async fn main() -> anyhow::Result<()> {
     let command = command.unwrap();
     match command {
         Command::Decode => {
-            let encoded_value = &args[2];
-            let decoded_value = bencode::decode_bencoded_value(encoded_value)?;
-            println!("{}", decoded_value);
+            let _ = execute_decode_command(&args[2]);
         }
         Command::Info => {
-            let file_path = &args[2];
-            let torrent = decode_torrent_file(file_path)?;
-            println!("Tracker URL: {}", torrent.announce);
-            println!("Length: {}", torrent.info.length);
-            println!("Info Hash: {}", torrent.info.hash_hex()?);
-
-            println!("Piece Length: {}", torrent.info.piece_length);
-            println!("Piece Hashes:");
-            torrent
-                .info
-                .pieces_hashes()?
-                .iter()
-                .for_each(|hash| println!("{}", hash));
+            let _ = execute_info_command(&args[2]);
         }
         Command::Peers => {
-            let file_path = &args[2];
-            let torrent = decode_torrent_file(file_path)?;
-            let tracker_response = tracker_get(&torrent).await?;
-            tracker_response
-                .peers()
-                .iter()
-                .for_each(|peer| println!("{peer}"));
+            let _ = execute_peers_command(&args[2]).await;
         }
         Command::Handshake => {
-            let file_path = &args[2];
-            let torrent = decode_torrent_file(file_path)?;
-            let tracker_response = tracker_get(&torrent).await?;
-            let peers = tracker_response.peers();
-            let target_peer = peers.first().unwrap();
-            let info_hash = torrent.info.hash_bytes()?;
-            let peer_id = handshake(target_peer, &info_hash).await?;
-            println!("Peer ID: {}", peer_id);
+            let _ = execute_handshake_command(&args[2]).await;
         }
     }
 
+    Ok(())
+}
+
+// ---
+// Commands bodies
+
+fn execute_decode_command(encoded_value: &str) -> anyhow::Result<()> {
+    let decoded_value = bencode::decode_bencoded_value(encoded_value)?;
+    println!("{}", decoded_value);
+    Ok(())
+}
+
+fn execute_info_command(file_path: &str) -> anyhow::Result<()> {
+    let torrent = decode_torrent_file(file_path)?;
+    println!("Tracker URL: {}", torrent.announce);
+    println!("Length: {}", torrent.info.length);
+    println!("Info Hash: {}", torrent.info.hash_hex()?);
+
+    println!("Piece Length: {}", torrent.info.piece_length);
+    println!("Piece Hashes:");
+    torrent
+        .info
+        .pieces_hashes()?
+        .iter()
+        .for_each(|hash| println!("{}", hash));
+    Ok(())
+}
+
+async fn execute_peers_command(file_path: &str) -> anyhow::Result<()> {
+    let torrent = decode_torrent_file(file_path)?;
+    let tracker_response = tracker_get(&torrent).await?;
+    tracker_response
+        .peers()
+        .iter()
+        .for_each(|peer| println!("{peer}"));
+    Ok(())
+}
+
+async fn execute_handshake_command(file_path: &str) -> anyhow::Result<()> {
+    let torrent = decode_torrent_file(file_path)?;
+    let tracker_response = tracker_get(&torrent).await?;
+    let peers = tracker_response.peers();
+    let target_peer = peers.first().unwrap();
+    let info_hash = torrent.info.hash_bytes()?;
+    let peer_id = handshake(target_peer, &info_hash).await?;
+    println!("Peer ID: {}", peer_id);
     Ok(())
 }
