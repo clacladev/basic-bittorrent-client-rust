7a2496a1f75b9807e1d7f30fb0f2088ada0cc1a7
Claudio Carnino <claudio@tugulab.org>
2023-12-23 20:24:13 +0000

    Removed the PeerMessageId entity since dirty

--- src/torrent_client/peer_messages.rs
+++ src/torrent_client/peer_messages.rs
@@ -1,51 +1,31 @@
 use crate::torrent_client::error::Error;
 
 #[derive(Debug)]
-pub enum PeerMessageId {
-    Interested,
-    Bitfield,
-}
-
-impl PeerMessageId {
-    pub fn from(numeric_id: u8) -> anyhow::Result<Self> {
-        match numeric_id {
-            2 => Ok(Self::Interested),
-            5 => Ok(Self::Bitfield),
-            id => Err(anyhow::Error::msg(
-                Error::PeerMessageIdNotRecognized(id).to_string(),
-            )),
-        }
-    }
-
-    pub fn to_numeric(&self) -> u8 {
-        match self {
-            Self::Interested => 2,
-            Self::Bitfield => 5,
-        }
-    }
-}
-
-#[derive(Debug)]
 pub enum PeerMessage {
+    Unchoke,
     Interested,
     Bitfield(u8), // bitfield byte
 }
 
 impl PeerMessage {
-    pub fn id(&self) -> PeerMessageId {
+    pub fn id(&self) -> u8 {
         match self {
-            Self::Interested => PeerMessageId::Interested,
-            Self::Bitfield(_) => PeerMessageId::Bitfield,
+            Self::Unchoke => 1,
+            Self::Interested => 2,
+            Self::Bitfield(_) => 5,
         }
     }
 }
 
 impl PeerMessage {
-    pub fn from_bytes(numeric_id: u8, body: &[u8]) -> anyhow::Result<Self> {
-        let peer_message_id = PeerMessageId::from(numeric_id)?;
-        match peer_message_id {
-            PeerMessageId::Interested => Ok(Self::Interested),
-            PeerMessageId::Bitfield => Ok(Self::Bitfield(body[0])),
+    pub fn from_bytes(id: u8, body: &[u8]) -> anyhow::Result<Self> {
+        match id {
+            1 => Ok(Self::Unchoke),
+            2 => Ok(Self::Interested),
+            5 => Ok(Self::Bitfield(body[0])),
+            _ => Err(anyhow::Error::msg(
+                Error::PeerMessageIdNotRecognized(id).to_string(),
+            )),
         }
     }
 
@@ -55,7 +35,7 @@ impl PeerMessage {
                 let message_length: u32 = 1;
                 let mut bytes = vec![0u8; 5];
                 bytes[0..4].copy_from_slice(&message_length.to_be_bytes());
-                bytes[4] = self.id().to_numeric();
+                bytes[4] = self.id();
                 Some(bytes)
             }
             _ => None,
